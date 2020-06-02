494dad17a573ec5383306eebbdd7a0588137d810
eo7q9 <66334080+eo7q9@users.noreply.github.com>
2020-06-02 23:03:04 +0200

    Create .travis.yml

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # kdawblcvr0
+
+
---
+++ build_scripts/build_release_binary.sh
@@ -0,0 +1,33 @@
+# This script takes care of building your crate and packaging it for release
+
+set -ex
+
+main() {
+    local src=$(pwd) \
+          stage=
+
+    case $TRAVIS_OS_NAME in
+        linux)
+            stage=$(mktemp -d)
+            ;;
+        osx)
+            stage=$(mktemp -d -t tmp)
+            ;;
+    esac
+
+    test -f Cargo.lock || cargo generate-lockfile
+
+    # create binary
+    cargo build --release --target $TARGET
+
+    # pack it up
+    cp target/$TARGET/release/bouncyquencer $stage/
+
+    cd $stage
+    tar czf $src/$CRATE_NAME-$TRAVIS_TAG-$TARGET.tar.gz *
+    cd $src
+
+    rm -rf $stage
+}
+
+main
